1e71747602619058af8d81dd29d9caaeb678304f
Fran Zekan <zekan.fran369@gmail.com>
2020-09-23 21:20:47 +0200

    Add chakra links

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,84 @@
 import * as React from 'react';
+import {
+  Link as ChakraLink,
+  LinkProps as ChakraLinkProps,
+  Button,
+  ButtonProps as ChakraButtonProps,
+  IconButton,
+  IconButtonProps as ChakraButtonIconProps,
+} from '@chakra-ui/core';
+import NextLink from 'next/link';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+export interface LinkProps extends Omit<ChakraLinkProps, 'as'> {
+  as?: string;
+}
+
+const LinkingComponent: React.FC<{ as?: string; href: string }> = ({
+  as,
+  href,
+  children,
+  ...props
+}) => {
+  if (!as && href.startsWith('http')) {
+    return (
+      <a href={href} {...props}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <NextLink href={href} passHref as={as}>
+      {children}
+    </NextLink>
+  );
+};
+
+export const Link: React.FC<LinkProps> = ({ as, href, children, ...props }) => {
+  return (
+    // fix this
+    <LinkingComponent href={href || ''} as={as} {...props}>
+      <ChakraLink {...props}>{children}</ChakraLink>
+    </LinkingComponent>
+  );
+};
+
+export interface LinkButtonProps extends Omit<ChakraButtonProps, 'as'> {
+  href: string;
+  as?: string;
+}
+
+export const LinkButton: React.FC<LinkButtonProps> = ({
+  as,
+  href,
+  children,
+  ...props
+}) => {
+  return (
+    <NextLink href={href} passHref as={as}>
+      <Button as="a" {...props}>
+        {children}
+      </Button>
+    </NextLink>
+  );
+};
+
+export interface LinkButtonIconProps extends Omit<ChakraButtonIconProps, 'as'> {
+  href: string;
+  as?: string;
+}
+
+export const LinkIconButton: React.FC<LinkButtonIconProps> = ({
+  as,
+  href,
+  children,
+  ...props
+}) => {
+  return (
+    <NextLink href={href} passHref as={as}>
+      <IconButton as="a" {...props}>
+        {children}
+      </IconButton>
+    </NextLink>
+  );
 };
