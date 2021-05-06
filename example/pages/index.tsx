eb5d93065461a4a2f8d6ce90c5341d942fc52069
Fran Zekan <zekan.fran369@gmail.com>
2021-05-06 22:43:13 +0200

    Better example

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -44,21 +44,31 @@ const Home: NextPage = () => {
         label="This is just a button link"
         other={{ onClick: () => alert('click') }}
       />
+
+      <Heading size="md">{`IsExternal prop => [o -> /other, g -> https://google.com]`}</Heading>
+      <Row label="/o1" href="/other" other={{ isExternal: true }} />
+      <Row label="/g1" href="https://google.com" other={{ isExternal: true }} />
+      <Row label="/o0" href="/other" other={{ isExternal: !1 }} />
+      <Row label="/g0" href="https://google.com" other={{}} />
+      <Row label="/o?" href="/other" other={{}} />
+      <Row label="/g?" href="https://google.com" other={{}} />
+
+      <Heading size="md">Using "justLink" prop to style other stuff</Heading>
       <HStack>
         <Link href="/other" justLink>
           <Box shadow="md" borderRadius="md" p="5" cursor="pointer">
-            <Heading>Hello</Heading>
+            <Heading size="sm">Hello</Heading>
             <Text>World</Text>
           </Box>
         </Link>
         <Link href="https://google.com" justLink>
           <Box shadow="md" borderRadius="md" p="5" cursor="pointer">
-            <Heading>Google</Heading>
+            <Heading size="sm">Google</Heading>
           </Box>
         </Link>
         <Link href="https://google.com" justLink isExternal={false}>
           <Box shadow="md" borderRadius="md" p="5" cursor="pointer">
-            <Heading>Google but same tab</Heading>
+            <Heading size="sm">Google but same tab</Heading>
           </Box>
         </Link>
       </HStack>
