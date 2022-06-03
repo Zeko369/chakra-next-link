a63e5d469bbbe47bac50816f88454199f7833278
Fran Zekan <zekan.fran369@gmail.com>
2022-06-03 15:23:28 +0200

    Fix react@18 types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import {
   ButtonProps as ChakraButtonProps,
   IconButtonProps as ChakraButtonIconProps,
@@ -11,6 +12,8 @@ export type BaseProps = {
   nextAs?: string;
   isExternal?: boolean;
   nativeAnchor?: boolean;
+
+  children: ReactNode;
 };
 
 type ChakraSpecific = {
